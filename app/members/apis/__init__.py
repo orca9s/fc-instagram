a47a845b43bd615c3d1ea7bbaf81068edb47b296
sangmin <sang93423@gmail.com>
2018-08-07 14:53:04 +0900

    facebook_login 프론트 부분과 나누어서 진행중

--- app/members/apis/__init__.py
+++ app/members/apis/__init__.py
@@ -46,3 +46,30 @@ class AuthenticationTest(APIView):
         if request.user.is_authenticated:
             return Response(UserSerializer(request.user).data)
         raise NotAuthenticated('로그인 되어있지 않습니다.')
+
+class FacebookLogin(APIView):
+    def post(self, request):
+        facebook_id = request.data.get('facebook_id')
+        last_name = request.data.get('last_name')
+        first_name = request.data.get('first_name')
+        user, __ = User.objects.get_or_create(
+            username=facebook_id,
+            defaults={
+                'last_name': last_name,
+                'first_name': first_name,
+            }
+        )
+        token, __ = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+        }
+        return Response(data)
+        # if User.objects.filter(username=facebook_id).exists():
+        #     user = User.objects.get(username=facebook_id)
+        #
+        #   URL: /api/users/facebook-login/
+        # request.data에 'facebook_id'와 'name'이 올 것으로 예상
+        # 1. 전달받은 facebook_id에 해당하는 유저가 존재하면 해당 User에
+        # 2. 존재하지 않는다면 'first_name'과 'last_name'값을 추가로 사용해 생성한 User에
+        #   -> 해당하는 Token을 가져오가나 새로 생성해서 리턴
+        # 결과는 Postman으로 확인
--- app/members/urls/apis.py
+++ app/members/urls/apis.py
@@ -6,5 +6,6 @@ from .. import apis
 urlpatterns = [
     path('', apis.UserList.as_view()),
     path('auth-token/', apis.AuthToken.as_view()),
-    path('auth-test/', apis.AuthenticationTest.as_view())
+    path('auth-test/', apis.AuthenticationTest.as_view()),
+    path('facebook-login/', apis.FacebookLogin.as_view())
 ]
